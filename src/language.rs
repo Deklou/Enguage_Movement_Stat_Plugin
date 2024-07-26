2d01ea9f38a60330e7a91d84ed39404bab7d2dd2
Deklou <azek_nui@yahoo.fr>
2024-07-26 14:04:37 +0200

    add selection and confirmation with A

--- src/language.rs
+++ src/language.rs
@@ -2,6 +2,8 @@ use unity::prelude::*;
 use engage::menu::{config::{ConfigBasicMenuItem, ConfigBasicMenuItemSwitchMethods}, BasicMenuResult};
 use crate::interface::{get_current_language, set_language, reflect_language_setting, reload_messages};
 
+static mut PREVIEW_LANG: i32 = 1; // Default to English
+
 fn get_language_name(lang_id: i32) -> &'static str {
     match lang_id {
         0 => "Japanese",
@@ -44,22 +46,21 @@ fn get_localized_string(key: &str, lang_id: i32) -> &'static str {
     }
 }
 
-
 pub struct LanguageSettings;
 
 impl ConfigBasicMenuItemSwitchMethods for LanguageSettings {
     fn init_content(this: &mut ConfigBasicMenuItem) {
-        update_texts(this);
+        unsafe {
+            PREVIEW_LANG = get_current_language(); // Initialize with current language
+            update_texts(this);
+        }
     }
 
     extern "C" fn custom_call(this: &mut ConfigBasicMenuItem, _method_info: Option<&'static MethodInfo>) -> BasicMenuResult {
         unsafe {
-            let current_lang = get_current_language();
-            let new_lang = ConfigBasicMenuItem::change_key_value_i(current_lang, 0, 11, 1);
-            if current_lang != new_lang {
-                set_language(new_lang);
-                reflect_language_setting();
-                reload_messages();
+            let new_lang = ConfigBasicMenuItem::change_key_value_i(PREVIEW_LANG, 0, 11, 1);
+            if PREVIEW_LANG != new_lang {
+                PREVIEW_LANG = new_lang;
                 update_texts(this);
                 BasicMenuResult::se_cursor()
             } else {
@@ -70,25 +71,33 @@ impl ConfigBasicMenuItemSwitchMethods for LanguageSettings {
 
     extern "C" fn set_help_text(this: &mut ConfigBasicMenuItem, _method_info: Option<&'static MethodInfo>) {
         unsafe {
-            let current_language = get_current_language();
-            let help_text = get_localized_string("current_language", current_language);
-            let lang_name = get_language_name(current_language);
+            let help_text = get_localized_string("current_language", PREVIEW_LANG);
+            let lang_name = get_language_name(PREVIEW_LANG);
             this.help_text = format!("{}: {}", help_text, lang_name).into();
         }
     }
 
     extern "C" fn set_command_text(this: &mut ConfigBasicMenuItem, _method_info: Option<&'static MethodInfo>) {
         unsafe {
-            let current_language = get_current_language();
-            let command_text = get_language_name(current_language);
+            let command_text = get_language_name(PREVIEW_LANG);
             this.command_text = format!("{}", command_text).into();
         }
     }
 }
 
+extern "C" fn a_button_confirm(this: &mut ConfigBasicMenuItem, _method_info: Option<&'static MethodInfo>) -> BasicMenuResult {
+    unsafe {
+        set_language(PREVIEW_LANG);
+        reflect_language_setting();
+        reload_messages();
+        update_texts(this);
+        BasicMenuResult::se_cursor()
+    }
+}
+
 fn update_texts(this: &mut ConfigBasicMenuItem) {
     unsafe {
-        let lang_id = get_current_language();
+        let lang_id = PREVIEW_LANG;
         this.title_text = get_localized_string("change_language", lang_id).into();
         LanguageSettings::set_help_text(this, None);
         LanguageSettings::set_command_text(this, None);
@@ -99,7 +108,8 @@ fn update_texts(this: &mut ConfigBasicMenuItem) {
 #[no_mangle]
 extern "C" fn language_callback() -> &'static mut ConfigBasicMenuItem {
     let switch = ConfigBasicMenuItem::new_switch::<LanguageSettings>("Change Language");
-    update_texts(switch); // Make sure text is updated here as well
+    switch.get_class_mut().get_virtual_method_mut("ACall").map(|method| method.method_ptr = a_button_confirm as _);
+    update_texts(switch);
     switch
 }
 
